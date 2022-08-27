8fa2ca1e9120c8bdf4350da4026e69c4a0ebc6a2
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-27 19:47:09 +0200

    CRUD methods for userRouter have been prepared.

--- routes/users.router.ts
+++ routes/users.router.ts
@@ -1,3 +1,10 @@
 import {Router} from "express";
+import {deleteUser, getAllUsers, getOneUser, updateUser} from "../controllers/user.controller";
 
 export const usersRouter = Router();
+
+usersRouter
+    .put('/:id', updateUser)
+    .delete('/:id', deleteUser)
+    .get('/:id', getOneUser)
+    .get('/', getAllUsers);
