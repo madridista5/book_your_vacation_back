01f00d2fe85de085e49a4ecc6e847d8d79b192cf
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-27 16:35:24 +0200

    Hash password was created (bcryptjs).

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,12 +1,16 @@
 import {NextFunction, Request, Response} from "express";
 import User from "../models/User";
+import bcrypt from 'bcryptjs';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            hashPwd: hash,
         });
         await newUser.save();
         res.status(200).json('Użytkownik został utworzony.');
