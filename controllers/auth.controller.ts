a9a57ee934cdef05780977bfcf8a0dd7dd7b7567
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-27 17:25:12 +0200

    Method login was created.

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import User from "../models/User";
 import bcrypt from 'bcryptjs';
+import {createError} from "../utils/error";
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -14,6 +15,26 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
         });
         await newUser.save();
         res.status(200).json('Użytkownik został utworzony.');
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        if(!user) return next(createError(404, "Użytkownik nie został znaleziony."));
+
+        const isPassCorrect = await bcrypt.compare(req.body.password, user.hashPwd);
+        if(!isPassCorrect) return next(createError(400, "Nieprawidłowe hasło lub login."));
+
+        const userResponse = {
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+        };
+
+        res.status(200).json(userResponse);
     } catch (err) {
         next(err);
     }
